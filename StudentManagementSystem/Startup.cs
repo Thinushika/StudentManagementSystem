120f5950de7f6eac06e6fc0a6575a4d628774f72
Thinushi <thinupavani@gmail.com>
2022-04-02 08:55:36 +0530

    Serializer created

--- StudentManagementSystem/Startup.cs
+++ StudentManagementSystem/Startup.cs
@@ -9,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Serialization;
 
 namespace StudentManagementSystem
 {
@@ -29,6 +30,11 @@ namespace StudentManagementSystem
             {
                 c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             });
+
+            //JSON Serializer
+            services.AddControllersWithViews().AddNewtonsoftJson(options =>
+            options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore).AddNewtonsoftJson(options => 
+            options.SerializerSettings.ContractResolver = new DefaultContractResolver());
 
             services.AddControllers();
         }
