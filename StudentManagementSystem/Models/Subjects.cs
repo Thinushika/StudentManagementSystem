37e43a9b226bc276e968651e8426e8ab73c85507
Thinushi <thinupavani@gmail.com>
2022-04-02 08:56:35 +0530

    Subject model created

---
+++ StudentManagementSystem/Models/Subjects.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentManagementSystem.Models
+{
+    public class Subjects
+    {
+        public int SubjectsId { get; set; }
+        public string SubjectName { get; set; }
+        public Classes ClassId { get; set; }
+        public Teacher TeacherId { get; set; }
+    }
+}
