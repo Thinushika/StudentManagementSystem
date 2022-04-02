bc4171b26628a09726620df4ca311a58cd3c8b1c
Thinushi <thinupavani@gmail.com>
2022-04-02 08:56:21 +0530

    Student model created

---
+++ StudentManagementSystem/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿namespace StudentManagementSystem.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentFirstName { get; set; }
+        public string StudentLastName { get; set; }
+        public string ContactPerson { get; set; }
+        public string ContactNumber { get; set; }
+        public string StudentEmail { get; set; }
+        public string StudentDOB { get; set; }
+        public int Age { get; set; }
+        public string EnrolmentDate { get; set; }
+        public Classes ClassID { get; set; }
+
+    }
+}
