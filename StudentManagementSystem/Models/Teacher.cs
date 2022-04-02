949935ed44b903f1c76ab9dd19e62f79468460c5
Thinushi <thinupavani@gmail.com>
2022-04-02 08:56:50 +0530

    Teacher model created

---
+++ StudentManagementSystem/Models/Teacher.cs
@@ -0,0 +1,13 @@
+﻿namespace StudentManagementSystem.Models
+{
+    public class Teacher
+    {
+        public int TeacherId { get; set; }
+        public string TeacherFirstName { get; set; }
+        public string TeacherLastName { get; set; }
+        public string ContactNumber { get; set; }
+        public string TeacherEmail { get; set; }
+        public string EnrolmentDate { get; set; }
+        public Subjects SubjectsId { get; set; }
+    }
+}
